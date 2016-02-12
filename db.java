2f10c4f9f0ddce1df140badf45ee4a69fa26cd80
PasichnykMV <mpasichnyk220@gmail.com>
2016-02-12 11:06:17 +0200

    Second commit into branch

---
+++ db.java
@@ -0,0 +1,12 @@
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
+
+public class db {
+
+   /* public static void main(String[] args) throws ClassNotFoundException, SQLException, FileNotFoundException {
+        Connect.Connect();
+        Connect.CreateDB();
+        Connect.WriteDB();
+        Connect.CloseDB();
+    }*/
+}
